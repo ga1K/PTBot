56fabe0ac7b1f34c12604fd06eb2155724edbfcb
gaika <gaika20055@gmail.com>
2023-03-28 11:10:04 +0900

    Первые тесты с ботом

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
 import telebot
 from telebot import types
-token='наш токен'
+token = '5734387053:AAE_C1Ol4pqMSL7IqbzRo4H1Ey0IcwETusI'
 bot=telebot.TeleBot(token)
 @bot.message_handler(commands=['start'])
 def start_message(message):
-	bot.send_message(message.chat.id,'Привет')
+    bot.send_message(message.chat.id,'Привет')
 @bot.message_handler(commands=['button'])
 def button_message(message):
-	markup=types.ReplyKeyboardMarkup(resize_keyboard=True)
-	item1=types.KeyboardButton("Кнопка")
+    markup=types.ReplyKeyboardMarkup(resize_keyboard=True)
+    item1=types.KeyboardButton("Кнопка")
+    markup.add(item1)
+    bot.send_message(message.chat.id,'Выберите что вам надо',reply_markup=markup)
+@bot.message_handler(content_types='text')
+def message_reply(message):
+    if message.text=="Кнопка":
+        bot.send_message(message.chat.id,"https://habr.com/ru/users/lubaznatel/")
+bot.infinity_polling()
