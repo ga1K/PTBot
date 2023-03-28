b71b8796837c64bad4ef609925f66a12c79d22f3
gaika <gaika20055@gmail.com>
2023-03-28 10:51:52 +0900

    first test

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print('hell1o')
+import telebot
+from telebot import types
+token='наш токен'
+bot=telebot.TeleBot(token)
+@bot.message_handler(commands=['start'])
+def start_message(message):
+	bot.send_message(message.chat.id,'Привет')
+@bot.message_handler(commands=['button'])
+def button_message(message):
+	markup=types.ReplyKeyboardMarkup(resize_keyboard=True)
+	item1=types.KeyboardButton("Кнопка")
